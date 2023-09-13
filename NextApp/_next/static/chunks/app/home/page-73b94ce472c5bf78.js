b75f0d034d8da4c22dd0e976595b2188ecb0831f
olovhaggstrom <90260315+olovhaggstrom@users.noreply.github.com>
2023-09-13 15:54:31 +0200

    Inital commit

---
+++ NextApp/_next/static/chunks/app/home/page-73b94ce472c5bf78.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[951],{232:function(e,t,n){Promise.resolve().then(n.bind(n,7251)),Promise.resolve().then(n.t.bind(n,1192,23))},7251:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return _}});var r=n(7437),o=n(2265),i=n(2722),c=n.n(i);function _(){let[e,t]=(0,o.useState)(!1),n=e?c().active:c().inactive,i=e?"\uD83D\uDD25 Deactivate me":"\uD83D\uDE34 Activate me";return(0,r.jsx)("button",{className:n,onClick:function(){t(!e)},children:i})}},1192:function(e){e.exports={main:"page_main__GlU4n",description:"page_description__86bsR",canvas:"page_canvas__dt_l4",center:"page_center__5oHG7"}},2722:function(e){e.exports={active:"toggle-button_active__ktXEJ",inactive:"toggle-button_inactive__WrUaf"}},622:function(e,t,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var r=n(2265),o=Symbol.for("react.element"),i=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),c=r.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,_={key:!0,ref:!0,__self:!0,__source:!0};t.jsx=function(e,t,n){var r,a={},u=null,s=null;for(r in void 0!==n&&(u=""+n),void 0!==t.key&&(u=""+t.key),void 0!==t.ref&&(s=t.ref),t)i.call(t,r)&&!_.hasOwnProperty(r)&&(a[r]=t[r]);if(e&&e.defaultProps)for(r in t=e.defaultProps)void 0===a[r]&&(a[r]=t[r]);return{$$typeof:o,type:e,key:u,ref:s,props:a,_owner:c.current}}},7437:function(e,t,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=232)}),_N_E=e.O()}]);
---
+++ NextApp/_next/static/chunks/app/home/variant-2/page-834e8a11c2f7bbcd.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[759],{5526:function(e,t,n){Promise.resolve().then(n.bind(n,7251)),Promise.resolve().then(n.t.bind(n,1192,23)),Promise.resolve().then(n.t.bind(n,4173,23))},7251:function(e,t,n){"use strict";n.r(t),n.d(t,{default:function(){return c}});var r=n(7437),o=n(2265),i=n(2722),_=n.n(i);function c(){let[e,t]=(0,o.useState)(!1),n=e?_().active:_().inactive,i=e?"\uD83D\uDD25 Deactivate me":"\uD83D\uDE34 Activate me";return(0,r.jsx)("button",{className:n,onClick:function(){t(!e)},children:i})}},4173:function(e){e.exports={description:"page_description__dxpTu",canvas:"page_canvas__aXAQ8"}},1192:function(e){e.exports={main:"page_main__GlU4n",description:"page_description__86bsR",canvas:"page_canvas__dt_l4",center:"page_center__5oHG7"}},2722:function(e){e.exports={active:"toggle-button_active__ktXEJ",inactive:"toggle-button_inactive__WrUaf"}},622:function(e,t,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var r=n(2265),o=Symbol.for("react.element"),i=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),_=r.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};t.jsx=function(e,t,n){var r,a={},s=null,u=null;for(r in void 0!==n&&(s=""+n),void 0!==t.key&&(s=""+t.key),void 0!==t.ref&&(u=t.ref),t)i.call(t,r)&&!c.hasOwnProperty(r)&&(a[r]=t[r]);if(e&&e.defaultProps)for(r in t=e.defaultProps)void 0===a[r]&&(a[r]=t[r]);return{$$typeof:o,type:e,key:s,ref:u,props:a,_owner:_.current}}},7437:function(e,t,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,596,744],function(){return e(e.s=5526)}),_N_E=e.O()}]);
